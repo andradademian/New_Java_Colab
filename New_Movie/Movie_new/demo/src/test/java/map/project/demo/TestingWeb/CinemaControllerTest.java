fb15a77d2e516f40ca395618f98f4e68b915b675
Andrada <andradademian@gmail.com>
2024-01-08 17:28:50 +0200

    some more tests

--- New_Movie/Movie_new/demo/src/test/java/map/project/demo/TestingWeb/CinemaControllerTest.java
+++ New_Movie/Movie_new/demo/src/test/java/map/project/demo/TestingWeb/CinemaControllerTest.java
@@ -1,99 +1,99 @@
-//package map.project.demo.TestingWeb;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import map.project.demo.Controller.ActorController;
-//import map.project.demo.Controller.CinemaController;
-//import map.project.demo.Domain.Actor;
-//import map.project.demo.Domain.Cinema;
-//import map.project.demo.Repository.ActorRepository;
-//import map.project.demo.Repository.ICinemaRepo;
-//import org.checkerframework.checker.units.qual.C;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.util.Arrays;
-//import java.util.Vector;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.anyString;
-//import static org.mockito.Mockito.doNothing;
-//import static org.mockito.Mockito.when;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@WebMvcTest(CinemaController.class)
-//public class CinemaControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private ICinemaRepo cinemaRepository;
-//
-//    @InjectMocks
-//    private CinemaController cinemaController;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    @Test
-//    void testAddCinema() throws Exception {
-//        Cinema cinema = new Cinema("1KX", "Cinema City", "Str. Unirii", new Vector<>());
-//
-//        doNothing().when(cinemaRepository).addCinemaToTable(any(Cinema.class));
-//
-//        mockMvc.perform(post("/api/cinema")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(cinema)))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void testFindCinemaById() throws Exception {
-//        Cinema cinema = new Cinema("1KX", "Cinema City", "Str. Unirii", new Vector<>());
-//
-//        when(cinemaRepository.getCinemaWithIdFromTable("1")).thenReturn(cinema);
-//
-//        mockMvc.perform(get("/api/cinema/1"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.name").value("Cinema City"))
-//                .andExpect(jsonPath("$.address").value("Str. Unirii"));
-//    }
-//
-//    @Test
-//    void testDeleteCinemaWithId() throws Exception {
-//        doNothing().when(cinemaRepository).deleteCinemaWithIdFromTable(anyString());
-//        mockMvc.perform(delete("/api/cinema/{id}", "1"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void testDeleteAllCinemas() throws Exception {
-//        doNothing().when(cinemaRepository).deleteAllCinemasFromTable();
-//
-//        mockMvc.perform(delete("/api/cinema"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void testGetAllCinemas() throws Exception {
-//        Cinema cinema1 = new Cinema("1KX", "Cinema City", "Str. Unirii", new Vector<>());
-//        Cinema cinema2 = new Cinema("2KX", "Cinema Republica", "Piata Mihai Viteazu", new Vector<>());
-//
-//        Vector<Cinema> cinemas = new Vector<>(Arrays.asList(cinema1, cinema2));
-//
-//        when(cinemaRepository.getCinemasFromTable()).thenReturn(cinemas);
-//
-//        mockMvc.perform(get("/api/cinema"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.size()").value(2))
-//                .andExpect(jsonPath("$[0].name").value("Cinema City"))
-//                .andExpect(jsonPath("$[1].name").value("Cinema Republica"));
-//    }
-//}
+package map.project.demo.TestingWeb;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import map.project.demo.Controller.ActorController;
+import map.project.demo.Controller.CinemaController;
+import map.project.demo.Domain.Actor;
+import map.project.demo.Domain.Award;
+import map.project.demo.Domain.Cinema;
+import map.project.demo.Repository.IActorRepository;
+import map.project.demo.Repository.ICinemaRepo;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CinemaControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ICinemaRepo cinemaRepository;
+
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testAddCinema() throws Exception {
+        Cinema cinema = new Cinema("1KX", "Cinema City", "Str. Unirii", new Vector<>());
+
+        mockMvc.perform(post("/api/cinema")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cinema)))
+                .andExpect(status().isOk());
+
+        Optional<Cinema> savedCinema = cinemaRepository.findById("1KX");
+        assertTrue(savedCinema.isPresent());
+
+        Cinema retrievedCinema = savedCinema.get();
+        assertEquals("1KX", retrievedCinema.getId());
+
+        cinemaRepository.deleteById("1KX");
+    }
+
+    @Test
+    void testFindCinemaById() throws Exception {
+        Cinema cinema = new Cinema("1KX", "Cinema City", "Str. Unirii", new Vector<>());
+
+        mockMvc.perform(post("/api/cinema")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cinema)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/cinema/1KX"))
+                .andExpect(status().isOk());
+
+        cinemaRepository.deleteById("1KX");
+    }
+
+    @Test
+    void testDeleteCinemaWithId() throws Exception {
+        Cinema cinema = new Cinema();
+        cinema.setId("1KX");
+
+        mockMvc.perform(post("/api/cinema")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cinema)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(delete("/api/cinema/{id}", "1KX"))
+                .andExpect(status().isOk());
+
+        assertFalse(cinemaRepository.existsById("1KX"));
+    }
+
+
+}
--- New_Movie/Movie_new/demo/src/test/java/map/project/demo/TestingWeb/GenreControllerTest.java
+++ New_Movie/Movie_new/demo/src/test/java/map/project/demo/TestingWeb/GenreControllerTest.java
@@ -1,125 +1,99 @@
-//package map.project.demo.TestingWeb;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import map.project.demo.Controller.GenreController;
-//import map.project.demo.Domain.Actor;
-//import map.project.demo.Domain.Genre;
-//import map.project.demo.Repository.GenreRepository;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.util.Arrays;
-//import java.util.Vector;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.*;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-//import static org.mockito.Mockito.when;
-//
-//@WebMvcTest(GenreController.class)
-//public class GenreControllerTest {
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private GenreRepository genreRepository;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    @Test
-//    void testAddActor() throws Exception {
-//        Genre genre = new Genre();
-//        genre.setId("1");
-//        genre.setName("Drama");
-//
-//        doNothing().when(genreRepository).addGenreToTable(any(Genre.class));
-//
-//        mockMvc.perform(post("/api/genre")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(genre)))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void testFindGenreById() throws Exception {
-//        Genre genre = new Genre();
-//        genre.setId("1");
-//        genre.setName("Drama");
-//
-//        when(genreRepository.getGenreWithIdFromTable("1")).thenReturn(genre);
-//
-//        mockMvc.perform(get("/api/genre/1"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.name").value("Drama"));
-//    }
-//
-//    @Test
-//    void testDeleteGenreWithIdFromTable() throws Exception {
-//        doNothing().when(genreRepository).deleteGenreWithIdFromTable(anyString());
-//
-//        mockMvc.perform(delete("/api/genre/{id}", "1"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void testDeleteAllGenres() throws Exception {
-//        doNothing().when(genreRepository).deleteAllFromGenreTable();
-//
-//        mockMvc.perform(delete("/api/genre"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void testDeleteAllFromMovieGenreTable() throws Exception {
-//        doNothing().when(genreRepository).deleteAllMoviesFromGenreWithId(anyString());
-//
-//        mockMvc.perform(delete("/api/genre/{id}/movies", "1"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void testDeleteMovie() throws Exception {
-//        doNothing().when(genreRepository).deleteMovie(anyString(), anyString());
-//
-//        mockMvc.perform(delete("/api/genre/{genreId}/movies/{movieId}", "1", "Movie1"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void testAddMovie() throws Exception {
-//        doNothing().when(genreRepository).addMovie(anyString(), anyString());
-//
-//        mockMvc.perform(post("/api/genre/{genreId}/movies", "1")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("\"Movie1\""))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void testGetAllGenres() throws Exception {
-//        Genre genre1 = new Genre();
-//        genre1.setId("1");
-//        genre1.setName("Action");
-//
-//        Genre genre2 = new Genre();
-//        genre2.setId("2");
-//        genre2.setName("Comedy");
-//
-//        Vector<Genre> genres = new Vector<>(Arrays.asList(genre1, genre2));
-//
-//        when(genreRepository.getGenresFromTable()).thenReturn(genres);
-//
-//        mockMvc.perform(get("/api/genre"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.size()").value(2))
-//                .andExpect(jsonPath("$[0].name").value("Action"))
-//                .andExpect(jsonPath("$[1].name").value("Comedy"));
-//    }
-//}
+package map.project.demo.TestingWeb;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import map.project.demo.Controller.GenreController;
+import map.project.demo.Domain.Actor;
+import map.project.demo.Domain.Cinema;
+import map.project.demo.Domain.Genre;
+import map.project.demo.Repository.IGenreRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GenreControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private IGenreRepo genreRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testAddGenre() throws Exception {
+        Genre genre = new Genre();
+        genre.setId("1AB");
+        genre.setName("Drama");
+
+        mockMvc.perform(post("/api/genre")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(genre)))
+                .andExpect(status().isOk());
+
+        Optional<Genre> savedGenre = genreRepository.findById("1AB");
+        assertTrue(savedGenre.isPresent());
+
+        Genre retrievedGenre = savedGenre.get();
+        assertEquals("1AB", retrievedGenre.getId());
+
+        genreRepository.deleteById("1AB");
+    }
+
+    @Test
+    void testFindGenreById() throws Exception {
+        Genre genre = new Genre();
+        genre.setId("1AB");
+        genre.setName("Drama");
+
+        mockMvc.perform(post("/api/genre")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(genre)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/genre/1AB"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Drama"));
+
+        genreRepository.deleteById("1AB");
+
+
+    }
+
+    @Test
+    void testDeleteGenreWithIdFromTable() throws Exception {
+        Genre genre = new Genre();
+        genre.setId("1");
+
+        mockMvc.perform(post("/api/genre")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(genre)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(delete("/api/genre/{id}", "1"))
+                .andExpect(status().isOk());
+
+        assertFalse(genreRepository.existsById("1"));
+        genreRepository.deleteById("1");
+    }
+
+}
